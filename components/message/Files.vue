dc70c72cb43071ec1191d5585596c1b1e068f421
Shuqian Hon <honsq90+github@protonmail.com>
2023-10-01 10:45:29 +1100

    Inline media for file attachments

--- components/message/Files.vue
+++ components/message/Files.vue
@@ -12,21 +12,22 @@ defineProps<Props>()
 <template>
   <div class="p-2 flex gap-2">
     <div>
-      <FileIcon class="w-8 h-8" />
-    </div>
-    <div>
-      <div class="font-bold mb-2">
-        {{ $t("file", files.length) }}
-      </div>
       <ul v-for="file in files" :key="file.id">
         <li>
-          <a
-            v-if="'name' in file"
+          <video v-if="'mimetype' in file && file.mimetype.includes('video')"
+                 :src="`/${file.url_private}`"
+                 :type="file.mimetype"
+                 controls
+          />
+          <img v-else-if="'mimetype' in file && file.mimetype.includes('image')"
+               :src="`/${file.url_private}`"/>
+          <a v-else-if="'name' in file"
             class="fancy-link"
             :href="`/${file.url_private}`"
             target="_blank"
             rel="noopener noreferrer"
           >
+            <FileIcon class="w-8 h-8" />
             <span>{{ file.name }}</span>
           </a>
           <span v-else>{{ file.mode }}</span>
