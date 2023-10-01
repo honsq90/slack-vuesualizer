434a79e1ae63dfd903f6e20593eddaf0d510f4f7
Shuqian Hon <honsq90+github@protonmail.com>
2023-10-01 13:34:30 +1100

    Display attachments inline instead of downloading

--- server/api/attachments/[filename]/index.get.ts
+++ server/api/attachments/[filename]/index.get.ts
@@ -2,12 +2,13 @@ import { mongo } from '~/server/utils/mongo';
 import type { Attachment } from "~/types/Attachment";
 
 export default defineEventHandler(async (event) => {
-  const filename = decodeURIComponent(event.context.params!.filename)
-  const db = await mongo(event.context.mongouuid)
-  const attachment = await db.collection<Attachment>('attachments').findOne({ filename })
+    const filename = decodeURIComponent(event.context.params!.filename);
+    const db = await mongo(event.context.mongouuid);
+    const attachment = await db.collection<Attachment>('attachments').findOne({ filename });
 
-  if (!attachment)
-    throw createError({ statusCode: 404, statusMessage: 'Attachment not found' })
-
-  return Buffer.from(attachment.data, 'base64');
-})
+    if (!attachment)
+        throw createError({ statusCode: 404, statusMessage: 'Attachment not found' });
+    appendResponseHeader(event, "Content-Disposition", "inline");
+    appendResponseHeader(event, "Content-Type", attachment.type);
+    return Buffer.from(attachment.data, "base64");
+});
