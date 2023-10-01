06eda07f5c01474d35f3a31fef0181a9e0318499
Shuqian Hon <honsq90+github@protonmail.com>
2023-10-01 11:44:51 +1100

    Save mime types

--- server/api/import/channel/[channel]/attachments/[filename]/index.post.ts
+++ server/api/import/channel/[channel]/attachments/[filename]/index.post.ts
@@ -1,9 +1,12 @@
 import { writeFile, stat } from "fs/promises";
+import { mongo } from '~~/server/utils/mongo'
+import mime from "mime-types";
 
 export default defineEventHandler(async (event) => {
   const channel = decodeURIComponent(event.context.params!.channel)
   const filename = decodeURIComponent(event.context.params!.filename)
   const files = await readMultipartFormData(event);
+  const db = await mongo(event.context.mongouuid)
 
   if (!files || files.length === 0) {
     throw createError({
@@ -13,9 +16,16 @@ export default defineEventHandler(async (event) => {
   }
 
   for (let i = 0; i < files.length; i++) {
-    if (files[i].name === 'file') {
-      const data = files[i].data;
+    let multiPartDatum = files[i];
+    if (multiPartDatum.name === 'file') {
+      const data = multiPartDatum.data;
+      const type = mime.lookup(filename);
+
       const filePath = `./public/attachments/${filename}`;
+      await db
+          .collection('attachments')
+          .insertOne({channel, data, filename, type})
+
       try {
         await stat(filePath);
       } catch (e) {
